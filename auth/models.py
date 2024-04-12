f55bef99eabc001ec3f604031a9e4d15cc172bae
Angel Portillo <angelportilloparra@gmail.com>
2024-04-11 20:08:09 -0400

    feat(auth): add basic fields to User model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    username_validator = UnicodeUsernameValidator()
+
+    uuid = models.UUIDField(primary_key=True)
+    username = models.CharField(
+        unique=True,
+        max_length=20,
+        validators=[username_validator],
+        error_messages="That Username already exists",
+    )
+    password = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, error_messages="That Email already exists")
+    is_active = models.BooleanField(
+        default=True, db_comment="Soft delete existing user when False"
+    )
+    is_email_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = "user"
+        verbose_name = "user"
+        verbose_name_plural = "users"
