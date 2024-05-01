94156f3f2f54b30aacc810cf38f781d6881164a5
Angel Portillo <angelportilloparra@gmail.com>
2024-05-01 16:26:08 -0400

    add(admin-panel): add user model in django admin panel

--- auth/admin.py
+++ auth/admin.py
@@ -1,6 +1,77 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from auth.forms import CustomUserChangeForm, CustomUserCreationForm
+
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    readonly_fields = ("uuid",)
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (
+            "User Data",
+            {
+                "fields": (
+                    "uuid",
+                    "username",
+                    "email",
+                    "password",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_email_verified",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+    # add_fieldsets = (
+    #     (
+    #         None,
+    #         {
+    #             "classes": ("wide",),
+    #             "fields": (
+    #                 "uuid",
+    #                 "username",
+    #                 "email",
+    #                 "password1",
+    #                 "password2",
+    #                 "is_staff",
+    #                 "is_active",
+    #                 "groups",
+    #                 "user_permissions",
+    #                 "created_at",
+    #                 "updated_at",
+    #                 "last_login_at",
+    #             ),
+    #         },
+    #     ),
+    # )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomUserAdmin)
